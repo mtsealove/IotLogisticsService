40df60435b969edab293e5a940d5f2586a4d1319
mtsealove <40752097+mtsealove@users.noreply.github.com>
2019-10-13 21:26:37 +0900

    회사 로그인 추가

--- Server/DBConn.js
+++ Server/DBConn.js
@@ -1,32 +1,32 @@
-const fs=require('fs');
-const mysql=require('sync-mysql');
+const fs = require('fs');
+const mysql = require('sync-mysql');
 /*MySQL 연동 쿼리 분리를 위해 
 callback이 필요 없는 sync-mysql 모듈 사용 */
 
-const connection=new mysql({
+const connection = new mysql({
     host: 'localhost',
     user: 'IotAdmin',   //Iot DB관리 로컬 계정
     password: fs.readFileSync('pw.dat', 'utf-8'),   //비밀번호는 파일로 따로 관리
     database: 'IoT'
 });
 //에러 코드 지정
-const NoResult={
-    Result:'NoResult'
+const NoResult = {
+    Result: 'NoResult'
 }
-const WrongInput= {
-    Result:'WrongInput'
+const WrongInput = {
+    Result: 'WrongInput'
 }
 
 
 //화물 조회
-exports.GetItem=function(InvoiceNum) {
+exports.GetItem = function (InvoiceNum) {
     //입력 값 검증
-    if(!InvoiceNum||(InvoiceNum.length<=0||InvoiceNum.length>14)) {
+    if (!InvoiceNum || (InvoiceNum.length <= 0 || InvoiceNum.length > 14)) {
         console.log(WrongInput);
         return WrongInput;
     }
-    else { 
-        const query=`select II.*, CC.Name as CompanyName from 
+    else {
+        const query = `select II.*, CC.Name as CompanyName from 
         (select I.*, D.* from (
         select A.*, S.Action from AItem A join Status as S
         on A.Status=S.ID
@@ -35,10 +35,9 @@ exports.GetItem=function(InvoiceNum) {
         on I.Driver=D.ID) II 
         join Courier CC
         on II.CompanyID=CC.ID;`;
-        var result=connection.query(query)[0];
-        if(result) {    //검색 결과 반환
-            result.Result='OK';
-            console.log(result);
+        var result = connection.query(query)[0];
+        if (result) {    //검색 결과 반환
+            result.Result = 'OK';
             return result;
         } else {    //검색 결과 없음
             console.log(NoResult);
@@ -48,16 +47,34 @@ exports.GetItem=function(InvoiceNum) {
 }
 
 //타임라인 조회
-exports.GetTimeLine=function(InvoiceNum){
-    if(!InvoiceNum||(InvoiceNum.length<=0||InvoiceNum.length>14)) {
+exports.GetTimeLine = function (InvoiceNum) {
+    if (!InvoiceNum || (InvoiceNum.length <= 0 || InvoiceNum.length > 14)) {
         console.log(WrongInput);
         return WrongInput;
     } else {
-        const query=`select * from timeline
+        const query = `select * from timeline
         where InvoiceNum='${InvoiceNum}'
         order by WorkDate asc, WorkTime asc;`
-        var result=connection.query(query);
+        var result = connection.query(query);
         console.log(result);
         return result;
+    }
+}
+
+//기업 로그인
+exports.Login = function (ID, password) {
+    if(ID&&password){
+        const query=`select ID, Name from Courier
+        where ID='${ID}'
+        and password='${password}'`;
+        var result=connection.query(query)[0];
+        if(result){
+            result.Result='OK';
+            return result;
+        } else {
+            return NoResult;
+        }
+    } else {
+        return WrongInput;
     }
 }
--- Server/Main.js
+++ Server/Main.js
@@ -5,12 +5,23 @@ const port = 3700; //서버의 포트 번호
 const DB = require('./DBConn');
 const fs = require('fs');
 const InputCheck = require('./InputCheck');
+const session = require('express-session');
 
 app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.urlencoded({ extended: true, limit:'150mb' }));
 app.set('view engine', 'ejs');
 app.set('views', 'Views');
 app.use(express.static('Src'));
+app.use(body_parser.json());
+app.use(session({   //세션 설정
+    key: 'sid',
+    secret: 'secret',
+    resave: 'false',
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 10  //로그인 유지 시간(10시간)
+    }
+}));
 
 //배경 이미지 라우팅
 app.get('/background', (req, res) => {
@@ -58,19 +69,49 @@ app.get('/Personal', (req, res) => {
     }
     else { //모든 것이 정상적으로 입력된 상태
         //결과 페이지 표시
-        let result=DB.GetItem(InvoiceNum);
-        if(result.Result=='OK') {
-            let TimeLine=DB.GetTimeLine(InvoiceNum);
+        let result = DB.GetItem(InvoiceNum);
+        if (result.Result == 'OK') {
+            let TimeLine = DB.GetTimeLine(InvoiceNum);
             console.log('타임라인');
             console.log(TimeLine);
-            res.render('Personal_result', {'title':'개인 택배 조회', 'current':1, 'content':result, 'timeline':TimeLine});
-        console.log('normal');
+            res.render('Personal_result', { 'title': '개인 택배 조회', 'current': 1, 'content': result, 'timeline': TimeLine });
+            console.log('normal');
         } else {
             InvoiceError(res);
         }
-        
     }
-})
+});
+//기업용 조회 페이지
+app.get('/Company', (req, res) => {
+    console.log(req.session);
+    //세션이 존재하는지 확인
+    if (req.session.userID) {
+        console.log('로그인됨');
+        res.render('Company_result', {'title':'기업 조회', 'current':2});
+    } else {
+        console.log('로그인 안됨');
+        res.render('Company_login', { 'title': '로그인', 'current': 2 });
+    }
+});
+
+//기업 회원 로그인
+app.post('/Company/Loign', (req, res) => {
+    const userID=req.body['ID'];
+    const password=req.body['password'];
+    console.log(userID);
+    console.log(password);
+    //로그인 성공
+    const data=DB.Login(userID, password);
+    if(data.Result=='OK'){
+        console.log(data);
+        req.session.userID=data.ID;
+        req.session.userName=data.Name;
+        res.redirect('/Company');
+    } //실패
+    else {
+        res.send('<script>alert("ID와 비밀번호를 확인하세요");history.go(-1)</script>');
+    }
+});
 
 //송장번호 오류 페이지 출력
 function InvoiceError(res) {
