2e3a568692d8809857b7d0a7032194059114fc98
mtsealove <40752097+mtsealove@users.noreply.github.com>
2019-10-25 18:37:52 +0900

    안드로이드 서버 갱신 전반적인 기능 작성

--- Server/DBConn.js
+++ Server/DBConn.js
@@ -82,13 +82,18 @@ exports.Login = function (ID, password) {
 }
 
 //기사별 화물 목록 조회
-exports.GetItemByDriver = function (driver_id) {
+exports.GetItemByDriver = function (driver_id, sort) {
+    const statusQuery = `select status from driver where id='${driver_id}'`;
+    const Status = connection.query(statusQuery)[0].status;
+
     if (driver_id) {
         //배송이 완료되지 않은 화물만 출력
-        const query = `select * from AItem where Driver='${driver_id}' and status!=5`;
+        var query = `select * from AItem a join  Status s on a.status=s.ID where Driver='${driver_id}' and status!=6`;
+        if (sort) query += ` order by ${sort}`;
         var result = {};
         const data = connection.query(query);
         if (data) {
+            result.Status = Status;
             result.Result = 'OK';
             result.data = data;
             return result;
@@ -100,7 +105,7 @@ exports.GetItemByDriver = function (driver_id) {
     }
 }
 //기사 정보 출력
-exports.GetItemByDriver = function (driver_id) {
+exports.GetDriver = function (driver_id) {
     if (driver_id) {
         const query = `select * from driver d join status s 
         on d.status = s.id
@@ -115,3 +120,100 @@ exports.GetItemByDriver = function (driver_id) {
         return WrongInput;
     }
 }
+
+//안드로이드
+
+const reusltOk = {
+    Result: 'OK'
+};
+const resultErr = {
+    Result: 'Error'
+}
+
+//기사 정보 반환
+exports.GetDriverJson = function (driver_id, password) {
+    const error = {
+        ID: null,
+        DriverName: null,
+        DriverPhone: null,
+        CourierID: null,
+        Status: null
+    }
+    if (driver_id && password) {
+        const query = `select ID, DriverName, DriverPhone, CourierID, Status 
+        from Driver where ID='${driver_id}' and Password='${password}'`;
+        const data = connection.query(query)[0];
+        if (data) return data;
+        else return error;
+    } else {
+        return error;
+    }
+}
+
+exports.UpdateLocation = function (driver_id, address) {
+    const query = `update Aitem set Location='${address}'
+    where Driver='${driver_id}' 
+    and Status!=5`;
+    try {
+        connection.query(query);
+        return reusltOk;
+    } catch (err) {
+        console.error(err);
+        return resultErr;
+    }
+}
+
+exports.UpdateDriverStatus = function (driver_id, status) {
+    const query = `update Driver set status=${status}
+    where id='${driver_id}'`;
+
+    try {
+        connection.query(query);
+        return reusltOk;
+    } catch (err) {
+        console.error(err);
+        return resultErr;
+    }
+}
+
+exports.UpdateAitemStatus = function (invoice, status) {
+    const query = `update Aitem set status=${status} where invoicenum='${invoice}'`;
+    try {
+        connection.query(query);
+        return reusltOk;
+    } catch (err) {
+        console.error(err);
+        return resultErr;
+    }
+}
+
+//하나의 화물 정보 얻어오기
+exports.GetItemOnly = function (invoice) {
+    const query = `select * from Aitem where invoicenum='${invoice}'`;
+    const result = connection.query(query)[0];
+    if (result) return result;
+    else return resultErr;
+}
+
+
+//원하는 상태와 일치하는지 확인
+exports.GetDone = function (driver_id, status) {
+    //원하는 상태의 개수 확인
+    const query1 = `select count(*) as cnt from aitem
+    where status!=6 
+    and status=${status}
+    and driver='${driver_id}'`;
+
+    const cnt1 = connection.query(query1)[0].cnt;
+    console.log(cnt1);
+
+    //모든 상태 확인
+    const query2 = `select count(*) as cnt from aitem
+    where status!=6
+    and driver='${driver_id}'`;
+    const cnt2 = connection.query(query2)[0].cnt;
+    console.log(cnt2);
+
+    if (cnt1 == cnt2) return reusltOk;
+    else return resultErr;
+}       
